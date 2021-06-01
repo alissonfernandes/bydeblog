695d34893130719df83c304d2a0c551023ddb57d
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-06-01 20:39:37 -0300

    Init
    Início do projeto.

---
+++ bydeblog/src/main/java/br/com/spring/bydeblog/BydeblogApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.bydeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BydeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BydeblogApplication.class, args);
+	}
+
+}
---
+++ bydeblog/src/test/java/br/com/spring/bydeblog/BydeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.bydeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BydeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
