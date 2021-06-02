275ac14cca0ab2921abb93d0d37bafffc6e8946d
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-06-01 21:22:03 -0300

    Create-model
    Criando o modelo Post.

---
+++ bydeblog/src/main/java/br/com/spring/bydeblog/model/Post.java
@@ -0,0 +1,83 @@
+package br.com.spring.bydeblog.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "POST")
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column
+    private String title;
+    
+    @Column
+    private String author;
+    
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDate date;
+    
+    @Column
+    @Lob
+    private String text;
+
+    public Post() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" + "id=" + id + ", title=" + title + ", author=" + author + ", date=" + date + ", text=" + text + '}';
+    }
+    
+}
