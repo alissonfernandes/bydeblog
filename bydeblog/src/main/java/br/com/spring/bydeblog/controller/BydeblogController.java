5339cb1cb30e69a7f7ebf833004eaa907d1c5058
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-06-03 15:59:28 -0300

    New-template
    Criando novo template "Post.html" e Mapeando o controller para este template.

--- bydeblog/src/main/java/br/com/spring/bydeblog/controller/BydeblogController.java
+++ bydeblog/src/main/java/br/com/spring/bydeblog/controller/BydeblogController.java
@@ -5,6 +5,8 @@ import br.com.spring.bydeblog.service.BydeblogService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,6 +15,7 @@ public class BydeblogController {
     @Autowired
     BydeblogService bydeblogService;
     
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public ModelAndView getPosts(){
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> posts = bydeblogService.findAll();
