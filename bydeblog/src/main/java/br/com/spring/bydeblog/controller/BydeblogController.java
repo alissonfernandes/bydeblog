9e994f6ce67721c998e1989716f77fdbdb2951a2
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-06-03 21:16:22 -0300

    Update-bydeblogController-newRequest
    Adiciona nova requisição, tendo um "Id" como parâmetro, para retornar um "post" específico.

--- bydeblog/src/main/java/br/com/spring/bydeblog/controller/BydeblogController.java
+++ bydeblog/src/main/java/br/com/spring/bydeblog/controller/BydeblogController.java
@@ -5,6 +5,7 @@ import br.com.spring.bydeblog.service.BydeblogService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +21,14 @@ public class BydeblogController {
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> posts = bydeblogService.findAll();
         modelAndView.addObject("posts", posts);
+        return modelAndView;
+    }
+    
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPost(@PathVariable("id") Long id){
+        ModelAndView modelAndView = new ModelAndView("postDetails");
+        Post post = bydeblogService.findById(id);
+        modelAndView.addObject("post", post);
         return modelAndView;
     }
     
