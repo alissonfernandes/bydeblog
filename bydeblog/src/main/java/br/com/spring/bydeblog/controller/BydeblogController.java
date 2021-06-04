d82a06db5cd3da096107cd9f42be5190cf093a0e
Alisson Fernandes <alissonfernandes200@gmail.com>
2021-06-04 16:04:19 -0300

    Publishing-posts
    Criando um formulário para publicação de posts.

--- bydeblog/src/main/java/br/com/spring/bydeblog/controller/BydeblogController.java
+++ bydeblog/src/main/java/br/com/spring/bydeblog/controller/BydeblogController.java
@@ -2,13 +2,17 @@ package br.com.spring.bydeblog.controller;
 
 import br.com.spring.bydeblog.model.Post;
 import br.com.spring.bydeblog.service.BydeblogService;
+import java.time.LocalDate;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class BydeblogController {
@@ -32,4 +36,18 @@ public class BydeblogController {
         return modelAndView;
     }
     
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+        return "postForm";
+    }
+    
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setDate(LocalDate.now());
+        bydeblogService.save(post);
+        return "redirect:/posts";
+    }
 }
